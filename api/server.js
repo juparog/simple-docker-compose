b0426b0be4a466f8fc63e1479e7db5386384f143
Juan Rodriguez <juangasca95@gmail.com>
2022-07-12 17:53:43 -0500

    Se configuro el cliente y la api de ejemplo

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 app.get('/saludar/:name', (req, res) => {
     console.log("sfgsdg");
